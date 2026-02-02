23ce23debec6f8ed6a504591f9e6648aca3d1bb2
jungminnking <jungminnhwang@gmail.com>
2026-02-02 12:02:25 -0600

    Forsummit

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,6 @@
-## Assignment 1
+### Assignment 1
 
-# Making sure I'm ready...
+# Making sure I'm ready for everything...
 import pandas as pd
 import numpy as np
 import scipy
@@ -27,36 +27,36 @@ px.line(train, x='Timestamp', y='trips')
 # I've tried; (1) Seasonal-Dampened Trend ES, (2) VAR (failed to run since each time components are non-stationary), and GAMs
 # Ended up using GAM
 
-# Generalized Additive Models (GAMs) with Prophet
+## Generalized Additive Models (GAMs) with Prophet
 # The reason I chosed Prophet: 
 # Given dataset dosen't include any exogenous variables (all time components) & thus Prophet is simpler in this case 
-# The scipt below follows largely the templates provided during the class 
 train_p = train[['Timestamp', 'trips']] 
 train_p.columns = ['ds', 'y']
 train_p.head()
 
-# Create and fit model
-m = Prophet(daily_seasonality=True) # Didn't consider changepoint_prior_scale (let it use default = 0.05); 
-# After some search, I've got to know that, Prophet automatically adds yearly and weekly seasonality, but not daily; So I set up daily seasonality equals true  
-m.fit(train_p)
+# Creating and fitting model
+model = Prophet(daily_seasonality=True) # Didn't consider changepoint_prior_scale (let it use default = 0.05); 
+# After some research, I've got to know that, Prophet automatically adds yearly and weekly seasonality, but not the daily
+# So I set up daily seasonality equals true  
+modelFit = model.fit(train_p)
 
-# Create an empty dataframe with dates for future periods (744) 
+# Creating an empty dataframe with dates for future periods (744) 
 # At this point, didn't feel like having to use test.csv
 future = m.make_future_dataframe(periods=744, freq = 'h')
 
-# Fill in dataframe wtih forecasts of `y` for the future periods
+# Filling in dataframe wtih forecasts of `y` for the future periods
 forecast = m.predict(future)
 forecast.head()
 
-# Plot the model together with the forecast
+# Plotting the model together with the forecast
 fig = m.plot(forecast)
 fig.show()
 
-# Plot the components of the forecast
+# Plotting the components of the forecast
 fig = m.plot_components(forecast)
 fig.show()
 
-# Raw forecast numbers
-forecast[['ds','yhat']].head()
-
+# Raw forecast numbers & Creating a vector
+pred = forecast[['ds','yhat']]
+pred.head()
 
